d516308fc501f35505706027d0b50bb877cf2313
Grzegorz Blaszczyk <grzegorz.blaszczyk@screen9.com>
2020-10-23 00:59:33 +0200

    updates dist dir

--- dist/video.es.js
+++ dist/video.es.js
@@ -17803,7 +17803,7 @@ var selectConfigs = {
     options: [['0.70', '70%'], ['0.80', '80%'], ['0.90', '90%'], ['1.00', '100%'], ['1.10', '110%'], ['1.20', '120%'], ['1.30', '130%'], ['1.40', '140%'], ['1.50', '150%']],
     "default": 3,
     parser: function parser(v) {
-      return v === '1.00' ? null : Number(v);
+      return Number(v);
     }
   },
   textOpacity: {
